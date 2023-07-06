cbfb98712d6758cd33c75060fc31008b398beee9
Bamigbade <webbminister25@gmail.com>
2023-07-06 23:15:39 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,24 @@
+//check the length of the string
+// let word = word.length;
+
+// function recursiveFunc(word){
+//     // loop through the half of the string
+// for(i=0; i >word/2; i++ ){
+
+// //write a condition to check if the first letter and the last letter are the same
+// if(word[i] === word[length-1-i]){
+// return "its a palindrome"
+//    }else return "its not a palindrome"
+// }
+// }
+
+// //call the function
+
+// const result = recursiveFunc(word);
+
+// //log the result
+// console.log(word);
+
+function recursiveFunc(word){
+    // const letters = word.length;
+    
